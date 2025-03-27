08625032642be4812edc6d87abdeec10a1824e9d
tybv1 <thomas.bourega@version1.com>
2025-03-27 23:04:42 +0000

    Including additional files in the git

---
+++ src/main/java/uk/gov/justice/laa/NewPortal/NewPortalApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.NewPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewPortalApplication.class, args);
+	}
+
+}
