dd6f77a49f90b18b8bd5d9524108cfb055d3ea62
pijian.liao <pijian.liao@version1.com>
2025-04-15 16:45:24 +0100

    STB-1315 add test folder; add jacoco

---
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/service/LoginServiceTest.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.portal.landingpage.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginServiceTest {
+
+    @Test
+    void buildAzureLoginUrl() {
+
+    }
+}
