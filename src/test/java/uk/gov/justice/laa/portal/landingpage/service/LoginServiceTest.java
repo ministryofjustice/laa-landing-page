2e520853c8e71070c1d35af8f4f154b861ebc423
pijian.liao <pijian.liao@version1.com>
2025-04-16 10:48:02 +0100

    STB-1315 add test coverage to github action

--- src/test/java/uk/gov/justice/laa/portal/landingpage/service/LoginServiceTest.java
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/service/LoginServiceTest.java
@@ -2,8 +2,6 @@ package uk.gov.justice.laa.portal.landingpage.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class LoginServiceTest {
 
     @Test
