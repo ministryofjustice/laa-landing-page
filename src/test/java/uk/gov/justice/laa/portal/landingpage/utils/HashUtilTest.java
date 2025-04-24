a930926e31f8e144087233c096ef94d44b184ff7
Suresh Kavali <suresh.kavali@justice.gov.uk>
2025-04-24 16:02:13 +0100

    STB-1939 - Added test coverage for newly added classes

---
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/utils/HashUtilTest.java
@@ -0,0 +1,38 @@
+package uk.gov.justice.laa.portal.landingpage.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class HashUtilTest {
+
+    @Test
+    public void hashAString() {
+        assertThat(HashUtil.sha256("test")).isEqualTo("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08");
+    }
+
+    @Test
+    public void hashUUID() {
+        UUID uuid = UUID.fromString("698815d2-5760-4fd0-bdef-54c683e91b26");
+        assertThat(HashUtil.sha256(uuid)).isEqualTo("4efb3caa44d53b15ef398fa622110166f63eadc9ad68f6f8954529c39b901889");
+    }
+
+    @Test
+    public void hashNullString() {
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
+            String s = null;
+            assertThat(HashUtil.sha256(s)).isEqualTo("4efb3caa44d53b15ef398fa622110166f63eadc9ad68f6f8954529c39b901889");
+        }).withMessage("Invalid input string for hashing");
+    }
+
+    @Test
+    public void hashUuid() {
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
+            UUID uuid = null;
+            assertThat(HashUtil.sha256(uuid)).isEqualTo("4efb3caa44d53b15ef398fa622110166f63eadc9ad68f6f8954529c39b901889");
+        }).withMessage("Invalid input value for hashing");
+    }
+}
