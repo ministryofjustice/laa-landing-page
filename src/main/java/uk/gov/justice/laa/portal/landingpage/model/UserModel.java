59fe1507b6b8c0b772ac0e5cdf01ab0bce8aebd5
pijian.liao <pijian.liao@version1.com>
2025-05-01 15:59:25 +0100

    STB-1323 checkstyle

--- src/main/java/uk/gov/justice/laa/portal/landingpage/model/UserModel.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/model/UserModel.java
@@ -1,6 +1,11 @@
 package uk.gov.justice.laa.portal.landingpage.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- src/main/java/uk/gov/justice/laa/portal/landingpage/repository/UserModelRepository.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/repository/UserModelRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.justice.laa.portal.landingpage.model.UserModel;
 
+/**
+ * Javadoc comment.
+ */
 @Repository
 public interface UserModelRepository extends JpaRepository<UserModel, Long> {
 }
