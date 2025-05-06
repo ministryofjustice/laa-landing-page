4b20d5e4b7ed8e914da453f50a8e184ad21e9e16
pijian.liao <pijian.liao@version1.com>
2025-05-06 16:51:24 +0100

    STB-1323 checkstyle

--- src/test/java/uk/gov/justice/laa/portal/landingpage/repository/UserModelRepositoryTest.java
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/repository/UserModelRepositoryTest.java
@@ -1,6 +1,10 @@
 package uk.gov.justice.laa.portal.landingpage.repository;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
