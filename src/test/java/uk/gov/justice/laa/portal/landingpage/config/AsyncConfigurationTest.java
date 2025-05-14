8cffbac8cde498581fe0ac96dd6749d911bb1906
pijian.liao <pijian.liao@version1.com>
2025-05-14 15:04:11 +0100

    STB-1555 AsyncConfiguration Test

--- src/test/java/uk/gov/justice/laa/portal/landingpage/config/AsyncConfigurationTest.java
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/config/AsyncConfigurationTest.java
@@ -1,26 +1,24 @@
 package uk.gov.justice.laa.portal.landingpage.config;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Async config test
+ */
 public class AsyncConfigurationTest {
 
-    ApplicationContextRunner context = new ApplicationContextRunner()
-            .withUserConfiguration(AsyncConfiguration.class);
-
     @Test
-    public void should_check_presence_of_example_service() {
-        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext( AsyncConfiguration.class )) {
-            Map<String,Object> beans = applicationContext.getBeansWithAnnotation(EnableAsync.class);
-            assertEquals(1, beans.size());
-            assertTrue(beans.containsKey("asyncConfiguration"));
+    public void asyncConfiguration() {
+        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AsyncConfiguration.class)) {
+            Map<String, Object> beans = applicationContext.getBeansWithAnnotation(EnableAsync.class);
+            assertThat(beans).hasSize(1);
+            assertThat(beans).containsKey("asyncConfiguration");
         }
     }
 }
