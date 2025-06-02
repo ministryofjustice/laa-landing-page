68080232788bd48135e3fb8502af9a467c040ebe
pijian.liao <pijian.liao@version1.com>
2025-06-02 14:07:59 +0100

    STB-1720 checkstyle

--- src/main/java/uk/gov/justice/laa/portal/landingpage/model/UserModel.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/model/UserModel.java
@@ -1,6 +1,12 @@
 package uk.gov.justice.laa.portal.landingpage.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
