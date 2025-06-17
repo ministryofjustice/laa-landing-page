fc142e27b7c036ae013cd56e8b5fc3b942142c86
pijian.liao <pijian.liao@version1.com>
2025-06-17 15:57:55 +0100

    STB-1742 fix format

--- src/main/java/uk/gov/justice/laa/portal/landingpage/service/EventService.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/service/EventService.java
@@ -26,17 +26,17 @@ public class EventService {
             %s: Audit event %s, by User %s with user id %s, %s
             """;
     private static final String CREATE_USER_TEMPLATE = """
-            New user % created, user id %, with role %s
+            New user %s created, user id %s, with role %s
             """;
     private static final String UPDATE_USER_ROLE_TEMPLATE = """
-            Existing user % updated, user id %, with new role %s
+            Existing user %s updated, user id %s, with new role %s
             """;
     private static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
     public void auditUserCreate(CurrentUserDto currentUserDto, User user, List<String> selectedRoles) {
         String userName = user.getGivenName() + " " + user.getSurname();
         String roles = String.join(", ", selectedRoles);
-        String description = CREATE_USER_TEMPLATE.formatted(user.getId(), userName, roles);
+        String description = String.format(CREATE_USER_TEMPLATE, user.getId(), userName, roles);
         Event createEvent = auditEvent(currentUserDto.getUserId(), currentUserDto.getName(), EventType.CREATE_USER, description);
         logEvent(createEvent);
     }
@@ -44,8 +44,8 @@ public class EventService {
     public void auditUpdateRole(CurrentUserDto currentUserDto, User user, List<String> selectedRoles) {
         String userName = user.getGivenName() + " " + user.getSurname();
         String roles = String.join(", ", selectedRoles);
-        String description = UPDATE_USER_ROLE_TEMPLATE.formatted(user.getId(), userName, roles);
-        Event createEvent = auditEvent(currentUserDto.getUserId(), currentUserDto.getName(), EventType.CREATE_USER, description);
+        String description = String.format(UPDATE_USER_ROLE_TEMPLATE, user.getId(), userName, roles);
+        Event createEvent = auditEvent(currentUserDto.getUserId(), currentUserDto.getName(), EventType.UPDATE_USER, description);
         logEvent(createEvent);
     }
 
@@ -62,7 +62,7 @@ public class EventService {
 
     protected void logEvent(Event event) {
         String dateTime = event.getCreatedDate().format(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
-        String eventString = EVENT_TEMPLATE.format(dateTime, event.getEventType().toString(),
+        String eventString = String.format(EVENT_TEMPLATE, dateTime, event.getEventType().toString(),
                 event.getUserName(), event.getUserId().toString(), event.getDescription());
         logger.info(eventString);
     }
