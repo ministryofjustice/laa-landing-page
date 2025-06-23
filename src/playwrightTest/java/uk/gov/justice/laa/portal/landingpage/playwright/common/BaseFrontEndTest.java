ccb50bb2b424a479377105a5eeae66eb028e7e11
Nitish-Kaur <nitish.kaur@justice.gov.uk>
2025-06-23 16:17:01 +0100

    Refactoring
    Refactoring
    
    Refactoring
    
    Refactoring

--- src/playwrightTest/java/uk/gov/justice/laa/portal/landingpage/playwright/common/BaseFrontEndTest.java
+++ src/playwrightTest/java/uk/gov/justice/laa/portal/landingpage/playwright/common/BaseFrontEndTest.java
@@ -6,59 +6,52 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import uk.gov.justice.laa.portal.landingpage.TestApplication;
 
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
- * base integration test
+ * Base class with login and logout for each test class
  */
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@EnableConfigurationProperties()
-@SpringBootTest(classes = TestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseFrontEndTest {
+    private static Playwright playwright;
+    private static Browser browser;
+    public static Page page;
+    public static Properties config;
 
-    @Value("${laa.landing.page.url}")
-    private String laaLandingPageUrl;
-
-    @Value("${laa.landing.page.user}")
-    private String laaLandingPageUser;
-
-    @Value("${laa.landing.page.password}")
-    private String laaLandingPagePassword;
-
-   /**
+    /**
      * Login to landing page
      */
     @BeforeAll
-    void setup() {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-            Page page = browser.newPage();
-            page.navigate(laaLandingPageUrl);
-            page.locator("[id='i0116']").fill(laaLandingPageUser);
-            page.waitForTimeout(3000);
-            page.getByText("Next").click();
-            page.waitForTimeout(2000);
-            page.locator("[id='i0118']").fill(laaLandingPagePassword);
-            page.waitForTimeout(2000);
-            page.getByText("Sign in").click();
-            page.waitForTimeout(2000);
-            page.getByText("Yes").click();
-            page.waitForTimeout(5000);
-            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+    static void setup() throws IOException {
+        config = new Properties();
+        try(InputStream stream = BaseFrontEndTest.class.getResourceAsStream("/playwright.properties")) {
+            config.load(stream);
         }
+
+        playwright = Playwright.create();
+        boolean headless = Boolean.parseBoolean(config.getProperty("app.playwright.headless", "true"));
+
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(headless));
+
+        page = browser.newPage();
+        page.navigate(config.getProperty("laa.landing.page.url"));
+        page.locator("[id='i0116']").fill(config.getProperty("laa.landing.page.user"));
+        page.getByText("Next").click();
+        page.locator("[id='i0118']").fill(config.getProperty("laa.landing.page.password"));
+        page.waitForTimeout(1000);
+        page.getByText("Sign in").click();
+        page.getByText("Yes").click();
     }
 
     /**
      * Logoff Landing Page
      */
     @AfterAll
-    void tearDownAll() {}
+    static void tearDown() {
+        page.close();
+        browser.close();
+        playwright.close();
+    }
 }
--- src/playwrightTest/java/uk/gov/justice/laa/portal/landingpage/playwright/pages/LoginPageTest.java
+++ src/playwrightTest/java/uk/gov/justice/laa/portal/landingpage/playwright/pages/LoginPageTest.java
@@ -3,8 +3,14 @@ package uk.gov.justice.laa.portal.landingpage.playwright.pages;
 import org.junit.jupiter.api.Test;
 import uk.gov.justice.laa.portal.landingpage.playwright.common.BaseFrontEndTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class LoginPageTest extends BaseFrontEndTest {
 
     @Test
-    public void loginPageTest() {}
+    public void loginPageTest() {
+        var landingPage = page.getByText("Legal Aid Agency Portal");
+        assertNotNull(landingPage, "Failed to find Legal Aid Agency Portal page");
+    }
 }
