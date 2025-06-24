6af72ab6b0a79b3d55cc9b929913b4387df78855
S N O W Y <49162275+snowy-snowdon@users.noreply.github.com>
2025-06-24 10:23:54 +0100

    Enhance user application selection with validation error handling and improve form feedback

--- src/main/java/uk/gov/justice/laa/portal/landingpage/forms/ApplicationsForm.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/forms/ApplicationsForm.java
@@ -2,9 +2,11 @@ package uk.gov.justice.laa.portal.landingpage.forms;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ApplicationsForm {
+    @NotNull(message = "At least one application must be selected")
     List<String> apps;
 }
