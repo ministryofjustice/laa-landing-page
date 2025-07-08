c1afd487a7cf6fca507e64f8ea186f0f8533ec33
pijian.liao <pijian.liao@version1.com>
2025-07-08 16:33:10 +0100

    STB-2129 test coverage

--- src/test/java/uk/gov/justice/laa/portal/landingpage/service/GraphApiServiceTest.java
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/service/GraphApiServiceTest.java
@@ -1,5 +1,8 @@
 package uk.gov.justice.laa.portal.landingpage.service;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import com.microsoft.graph.models.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,12 +12,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.justice.laa.portal.landingpage.utils.LogMonitoring;
 import uk.gov.justice.laa.portal.landingpage.utils.RestUtils;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mockStatic;
 
 @ExtendWith(MockitoExtension.class)
@@ -57,9 +64,19 @@ class GraphApiServiceTest {
     }
 
     @Test
-    void logsUser_fromGraph() {
+    void logoutUser_fromGraph_Ok() {
         service.logoutUser("token");
         String url = "https://graph.microsoft.com/v1.0/me/revokeSignInSessions";
         mockedRestUtils.verify(() -> RestUtils.postGraphApi(eq("token"), eq(url), any()));
+    }
+
+    @Test
+    void logoutUser_fromGraph_Error() {
+        String url = "https://graph.microsoft.com/v1.0/me/revokeSignInSessions";
+        given(RestUtils.postGraphApi(eq("token"), eq(url), any())).willAnswer(invocation -> { throw new Exception("abc msg"); });
+        ListAppender<ILoggingEvent> listAppender = LogMonitoring.addListAppenderToLogger(GraphApiService.class);
+        service.logoutUser("token");
+        List<ILoggingEvent> warningLogs = LogMonitoring.getLogsByLevel(listAppender, Level.ERROR);
+        assertThat(warningLogs.size()).isEqualTo(1);
     }
 }
