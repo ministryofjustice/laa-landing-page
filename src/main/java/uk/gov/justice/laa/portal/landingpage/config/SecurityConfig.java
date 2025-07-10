4fbb3c0bc2e9459ad980cbc6cd2b7f573f2c96eb
miya-pniel-moj <miya.pniel@justice.gov.uk>
2025-07-10 17:21:04 +0100

    add import

--- src/main/java/uk/gov/justice/laa/portal/landingpage/config/SecurityConfig.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 import uk.gov.justice.laa.portal.landingpage.entity.UserType;
 import uk.gov.justice.laa.portal.landingpage.service.AuthzOidcUserDetailsService;
 
