ac7c874aa593663454115d85d658b71cf6197a35
pijian.liao <pijian.liao@version1.com>
2025-07-10 10:41:37 +0100

    STB-2129 sign out via POST

--- src/main/java/uk/gov/justice/laa/portal/landingpage/controller/LoginController.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/controller/LoginController.java
@@ -96,7 +96,7 @@ public class LoginController {
         return "migrate";
     }
 
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public RedirectView logout(Authentication authentication, HttpSession session, @RegisteredOAuth2AuthorizedClient("azure") OAuth2AuthorizedClient authClient) {
         loginService.logout(authentication, authClient, session);
         return new RedirectView("/?message=logout");
