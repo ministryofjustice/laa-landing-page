c9dbbea4faa3676f6ac26ddd2acbac53082c77f5
pijian.liao <pijian.liao@version1.com>
2025-07-10 11:04:58 +0100

    STB-2218 make update user event also work for other fields

--- src/main/java/uk/gov/justice/laa/portal/landingpage/dto/UpdateUserAuditEvent.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/dto/UpdateUserAuditEvent.java
@@ -4,19 +4,21 @@ import uk.gov.justice.laa.portal.landingpage.entity.EventType;
 
 import java.util.List;
 
-public class UpdateRoleAuditEvent extends AuditEvent {
+public class UpdateUserAuditEvent extends AuditEvent {
 
     private final EntraUserDto user;
-    private final List<String> selectedRoles;
+    private final List<String> changedValues;
+    private final String field;
     private static final String UPDATE_USER_ROLE_TEMPLATE = """
-            Existing user %s updated, user id %s, with new role %s
+            Existing user %s updated, user id %s, with new %s %s
             """;
 
-    public UpdateRoleAuditEvent(CurrentUserDto currentUserDto, EntraUserDto user, List<String> selectedRoles) {
+    public UpdateUserAuditEvent(CurrentUserDto currentUserDto, EntraUserDto user, List<String> changedValues, String field) {
         this.userId = currentUserDto.getUserId();
         this.userName = currentUserDto.getName();
         this.user = user;
-        this.selectedRoles = selectedRoles;
+        this.changedValues = changedValues;
+        this.field = field;
     }
 
     @Override
@@ -27,7 +29,7 @@ public class UpdateRoleAuditEvent extends AuditEvent {
     @Override
     public String getDescription() {
         String userName = user.getFullName();
-        String roles = String.join(", ", selectedRoles);
-        return String.format(UPDATE_USER_ROLE_TEMPLATE, userName, user.getId(), roles);
+        String changedString = String.join(", ", changedValues);
+        return String.format(UPDATE_USER_ROLE_TEMPLATE, userName, user.getId(), field, changedString);
     }
 }
