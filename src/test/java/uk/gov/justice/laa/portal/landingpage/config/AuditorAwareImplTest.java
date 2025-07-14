d76d22f1308ecc76e2bc85084524c625d9873953
pijian.liao <pijian.liao@version1.com>
2025-07-14 10:24:08 +0100

    STB-2218 jpa config test

--- src/test/java/uk/gov/justice/laa/portal/landingpage/config/AuditorAwareImplTest.java
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/config/AuditorAwareImplTest.java
@@ -2,6 +2,7 @@ package uk.gov.justice.laa.portal.landingpage.config;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -28,7 +29,18 @@ class AuditorAwareImplTest {
     }
 
     @Test
-    void getCurrentAuditor_system() {
+    void getCurrentAuditor_system_no_authentication() {
+        AnonymousAuthenticationToken authentication = Mockito.mock(AnonymousAuthenticationToken.class);
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        AuditorAwareImpl auditorAware = new AuditorAwareImpl();
+        assertThat(auditorAware.getCurrentAuditor()).isPresent();
+        assertThat(auditorAware.getCurrentAuditor().get()).isEqualTo("System");
+    }
+
+    @Test
+    void getCurrentAuditor_system_no_attribute() {
         OAuth2AuthenticationToken authentication = Mockito.mock(OAuth2AuthenticationToken.class);
         SecurityContext securityContext = Mockito.mock(SecurityContext.class);
         OAuth2User principal = Mockito.mock(OAuth2User.class);
