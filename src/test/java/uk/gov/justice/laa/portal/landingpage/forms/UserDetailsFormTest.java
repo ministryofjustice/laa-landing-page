9e84b3f241a2fc3d56a64f75db26f0de246773b1
sankavi-moj <sankavi.mohanraj@justice.gov.uk>
2025-07-22 09:48:11 +0100

    update regex to prevent special characters at the start of name

--- src/test/java/uk/gov/justice/laa/portal/landingpage/forms/UserDetailsFormTest.java
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/forms/UserDetailsFormTest.java
@@ -178,6 +178,18 @@ class UserDetailsFormTest {
     }
 
     @Test
+    void specialCharacterAtTheStartOfName_shouldTriggerPatternViolation() {
+        UserDetailsForm form = new UserDetailsForm();
+        form.setFirmId("firm1");
+        form.setFirstName(" Mary Jane");
+        form.setLastName("-O'Neil");
+        form.setEmail("test@example.com");
+        form.setUserType(UserType.EXTERNAL_SINGLE_FIRM);
+        Set<ConstraintViolation<UserDetailsForm>> violations = validator.validate(form);
+        assertThat(violations.size()).isEqualTo(2);
+    }
+
+    @Test
     void noUserType_shouldTriggerNotNullViolation() {
         UserDetailsForm form = new UserDetailsForm();
         form.setFirmId("firm1");
