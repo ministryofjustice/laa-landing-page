d9a64c1f8823fe3713e026512d9c51cbef422f93
sankavi-moj <sankavi.mohanraj@justice.gov.uk>
2025-07-25 14:48:21 +0100

    STB-1907 Add pagination

--- src/main/java/uk/gov/justice/laa/portal/landingpage/service/InternalUserPollingService.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/service/InternalUserPollingService.java
@@ -42,6 +42,25 @@ public class InternalUserPollingService {
         }
 
         List<DirectoryObject> allUsers = response.getValue();
+        String nextLink = response.getOdataNextLink();
+        int page = 1;
+        while (nextLink != null) {
+            response = graphClient
+                    .groups()
+                    .byGroupId(pollingGroupId)
+                    .members()
+                    .withUrl(nextLink)
+                    .get();
+
+            if (response != null && response.getValue() != null) {
+                allUsers.addAll(response.getValue());
+                nextLink = response.getOdataNextLink();
+                page++;
+                logger.info("Page {} of users retrieved and added to list of users from entra group", page);
+            } else  {
+                break;
+            }
+        }
         List<DirectoryObject> newUsers = allUsers.stream()
                 .filter(u -> !existingUserOids.contains(UUID.fromString(u.getId())))
                 .toList();
