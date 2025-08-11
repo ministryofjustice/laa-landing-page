304cc0c44887411b42ca0154310c134115ac75ac
pijian.liao <pijian.liao@version1.com>
2025-08-11 15:22:09 +0100

    STB-2563 trim the input and input should be notblank

--- src/main/java/uk/gov/justice/laa/portal/landingpage/forms/FirmSearchForm.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/forms/FirmSearchForm.java
@@ -1,14 +1,17 @@
 package uk.gov.justice.laa.portal.landingpage.forms;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class FirmSearchForm {
 
-    @Size(min = 1, message = "Enter a firm name to search")
+    @NotBlank(message = "Enter a firm name to search")
     private String firmSearch;
 
     private String selectedFirmId;
+
+    public void setFirmSearch(String firmSearch) {
+        this.firmSearch = firmSearch.trim();
+    }
 }
