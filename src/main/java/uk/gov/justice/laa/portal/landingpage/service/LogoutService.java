8c545d2604ab86b2a7f7c7eed7f40341a08e21d4
S N O W Y <49162275+snowy-snowdon@users.noreply.github.com>
2025-08-12 11:21:01 +0100

    Add base URI configuration for Azure OAuth2 client

--- src/main/java/uk/gov/justice/laa/portal/landingpage/service/LogoutService.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/service/LogoutService.java
@@ -9,23 +9,24 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class LogoutService {
 
-    @Value("${AZURE_TENANT_ID}")
-    private String tenantId;
-
-    @Value("${BASE_URL}")
+    @Value("${spring.security.oauth2.client.registration.azure.base-uri}")
     private String baseUrl;
+
+    @Value("${spring.security.oauth2.client.registration.azure.tenant-id}")
+    private String tenantId;
 
     /**
      * Builds the Azure AD logout URL with post-logout redirect
+     * 
      * @return The complete Azure AD logout URL
      */
     public String buildAzureLogoutUrl() {
         String postLogoutRedirectUri = baseUrl + "/?message=logout";
-        
+
         return UriComponentsBuilder
-            .fromUri(URI.create("https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/logout"))
-            .queryParam("post_logout_redirect_uri", postLogoutRedirectUri)
-            .build()
-            .toUriString();
+                .fromUri(URI.create("https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/logout"))
+                .queryParam("post_logout_redirect_uri", postLogoutRedirectUri)
+                .build()
+                .toUriString();
     }
 }
