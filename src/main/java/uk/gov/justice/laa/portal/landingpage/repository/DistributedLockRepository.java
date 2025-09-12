f227080fcf1785e3dd434db43ee3a3635fba3179
sureshkavali-moj <suresh.kavali@justice.gov.uk>
2025-09-12 14:01:36 +0100

    STB-2818 - change warn to debug and adjust tx (#476)

--- src/main/java/uk/gov/justice/laa/portal/landingpage/repository/DistributedLockRepository.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/repository/DistributedLockRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.justice.laa.portal.landingpage.entity.DistributedLock;
 
@@ -28,7 +27,7 @@ public interface DistributedLockRepository extends JpaRepository<DistributedLock
                     @Param("lockedBy") String lockedBy);
 
     @Modifying
-    @Query("DELETE FROM DistributedLock l WHERE l.key = :key AND l.lockedBy = :lockedBy")
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Query("DELETE FROM DistributedLock l WHERE l.key = :key")
+    @Transactional
     void releaseLock(@Param("key") String key, @Param("lockedBy") String lockedBy);
 }
