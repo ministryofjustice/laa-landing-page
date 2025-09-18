6279a53b76abf12de435cfd47da4ec42ae62ddb5
pijian.liao <pijian.liao@version1.com>
2025-09-18 16:08:32 +0100

    STB-2807 Serializable

--- src/main/java/uk/gov/justice/laa/portal/landingpage/forms/RolesForm.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/forms/RolesForm.java
@@ -1,12 +1,12 @@
 package uk.gov.justice.laa.portal.landingpage.forms;
 
+import java.io.Serializable;
 import java.util.List;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-public class RolesForm implements java.io.Serializable {
+public class RolesForm implements Serializable {
 
     List<String> roles;
 }
