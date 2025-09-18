9adfc9c7cd7d403f4b0145d255e5bbba63ea93cf
pijian.liao <pijian.liao@version1.com>
2025-09-18 13:36:54 +0100

    STB-2908 cleancron doesnt work, use clean up task

--- src/main/java/uk/gov/justice/laa/portal/landingpage/service/SessionCleanupTask.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/service/SessionCleanupTask.java
@@ -1,15 +1,15 @@
 package uk.gov.justice.laa.portal.landingpage.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SessionCleanupTask {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     @Scheduled(fixedRate = 3600000) // every hour
     public void cleanExpiredSessions() {
