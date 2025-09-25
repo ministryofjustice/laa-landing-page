6e90a7bc80e3dab34352e711faefe7b03743a787
Suresh Kavali <suresh.kavali@justice.gov.uk>
2025-09-25 12:32:58 +0100

    STB-2919: Added test coverage

--- src/test/java/uk/gov/justice/laa/portal/landingpage/service/FirmServiceTest.java
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/service/FirmServiceTest.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -262,6 +261,44 @@ class FirmServiceTest {
             assertThat(result).extracting(FirmDto::getName)
                     .containsExactlyInAnyOrder("Test Firm 1", "Test Firm 2");
             verify(firmRepository).findByNameOrCodeContaining("Test");
+        }
+
+        @Test
+        void whenInternalUser_withSearchTerm_filtersFirms_should_return_in_ranking_order() {
+            // Given
+            List<Firm> searchResults = List.of(
+                    Firm.builder().id(UUID.randomUUID()).name("A Test Firm 1").code("TF1").build(),
+                    Firm.builder().id(UUID.randomUUID()).name("Test Firm 2").code("TF2").build()
+            );
+            when(firmRepository.findByNameOrCodeContaining("Test")).thenReturn(searchResults);
+
+            // When
+            List<FirmDto> result = firmService.getUserAccessibleFirms(internalUser, "Test");
+
+            // Then
+            assertThat(result).hasSize(2);
+            assertThat(result).extracting(FirmDto::getName)
+                    .containsExactlyInAnyOrder("Test Firm 2", "A Test Firm 1");
+            verify(firmRepository).findByNameOrCodeContaining("Test");
+        }
+
+        @Test
+        void whenInternalUser_withSearchTerm_code_filtersFirms_should_return_in_ranking_order() {
+            // Given
+            List<Firm> searchResults = List.of(
+                    Firm.builder().id(UUID.randomUUID()).name("Test Firm 1").code("12345").build(),
+                    Firm.builder().id(UUID.randomUUID()).name("Test Firm 2").code("1234").build()
+            );
+            when(firmRepository.findByNameOrCodeContaining("1234")).thenReturn(searchResults);
+
+            // When
+            List<FirmDto> result = firmService.getUserAccessibleFirms(internalUser, "1234");
+
+            // Then
+            assertThat(result).hasSize(2);
+            assertThat(result).extracting(FirmDto::getName)
+                    .containsExactlyInAnyOrder("Test Firm 2", "Test Firm 1");
+            verify(firmRepository).findByNameOrCodeContaining("1234");
         }
 
         @Test
