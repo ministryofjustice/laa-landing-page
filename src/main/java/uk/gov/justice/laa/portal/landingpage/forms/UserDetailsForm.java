dba0cc1c94b0025806df0609ecfdb2b8f0dbd54e
pijian.liao <pijian.liao@version1.com>
2025-10-03 12:22:42 +0100

    STB-2940 spaces in first last name

--- src/main/java/uk/gov/justice/laa/portal/landingpage/forms/UserDetailsForm.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/forms/UserDetailsForm.java
@@ -14,12 +14,12 @@ public class UserDetailsForm implements Serializable {
 
     @NotEmpty(message = "Enter a first name")
     @ConditionalSize(min = 2, max = 99, message = "First name must be between 2-99 characters")
-    @ConditionalPattern(regexp = "^[A-Za-z](?:[A-Za-z \\-']*[A-Za-z])?$", message = "First name must not contain numbers or special characters")
+    @ConditionalPattern(regexp = "^[A-Za-z](?:[A-Za-z\\-']*[A-Za-z])?$", message = "First name must not contain numbers or special characters")
     private String firstName;
 
     @NotEmpty(message = "Enter a last name")
     @ConditionalSize(min = 2, max = 99, message = "Last name must be between 2-99 characters")
-    @ConditionalPattern(regexp = "^[A-Za-z](?:[A-Za-z \\-']*[A-Za-z])?$", message = "Last name must not contain numbers or special characters")
+    @ConditionalPattern(regexp = "^[A-Za-z](?:[A-Za-z\\-']*[A-Za-z])?$", message = "Last name must not contain numbers or special characters")
     private String lastName;
 
     @NotEmpty(message = "Enter an email address")
