d438de26f3f1587da67d520c3b83e36a7d102587
sankavi-moj <sankavi.mohanraj@justice.gov.uk>
2025-10-15 15:43:01 +0100

    STB-2860 Revert hardcoding tech services error

--- src/main/java/uk/gov/justice/laa/portal/landingpage/service/LiveTechServicesClient.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/service/LiveTechServicesClient.java
@@ -128,57 +128,54 @@ public class LiveTechServicesClient implements TechServicesClient {
 
     @Override
     public void deleteRoleAssignment(UUID userId) {
-        String errorBody = "{\n  \"success\": false, \n  \"code\": \"INTERNAL_SERVER_ERROR\", \n  \"message\": \"Simulated Tech Services failure for deleteRoleAssignment\"\n}";
-        throw HttpServerErrorException.create(HttpStatus.INTERNAL_SERVER_ERROR,
-                "Simulated Tech Services failure", null, errorBody.getBytes(), null);
-//        EntraUser entraUser = null;
-//        try {
-//            entraUser = entraUserRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-//
-//            UpdateSecurityGroupsRequest request = UpdateSecurityGroupsRequest
-//                    .builder()
-//                    .groups(Collections.emptySet())
-//                    .build();
-//
-//            String uri = String.format(TECH_SERVICES_UPDATE_USER_GRP_ENDPOINT, laaBusinessUnit, entraUser.getEntraOid());
-//
-//            logger.info("TechServices DELETE groups: userId={}, entraOid={}, uri={}", userId, entraUser.getEntraOid(), uri);
-//            logger.info("Sending request to tech services to remove group memberships for deleting: {}", request);
-//
-//            String accessToken = getAccessToken();
-//            ResponseEntity<UpdateSecurityGroupsResponse> response = restClient
-//                    .patch()
-//                    .uri(uri)
-//                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .body(request)
-//                    .retrieve()
-//                    .toEntity(UpdateSecurityGroupsResponse.class);
-//
-//            if (response.getStatusCode().is2xxSuccessful()) {
-//                UpdateSecurityGroupsResponse responseBody = response.getBody();
-//                assert responseBody != null;
-//                logger.info("Security Groups removed successfully for {} {} with security groups removed",
-//                        entraUser.getFirstName() + " " + entraUser.getLastName(), responseBody.getGroupsRemoved());
-//            } else {
-//                logger.error("Failed to remove security groups for user {} with error code {}", entraUser.getFirstName() + " " + entraUser.getLastName(), response.getStatusCode());
-//                throw new RuntimeException("Failed to remove security groups for user " + entraUser.getFirstName() + " " + entraUser.getLastName() + " with error code " + response.getStatusCode());
-//            }
-//        } catch (HttpClientErrorException | HttpServerErrorException httpEx) {
-//            String errorJson = httpEx.getResponseBodyAsString();
-//            // Do not throw error for 404, user may already be deleted in entra
-//            if (httpEx.getStatusCode() == HttpStatus.NOT_FOUND) {
-//                logger.warn("Tech Services DELETE groups returned 404 Not Found for userId={}, entraOid={}. User will be deleted from Silas db. Body={}",
-//                        userId, (entraUser != null ? entraUser.getEntraOid() : null), errorJson);
-//                return;
-//            }
-//            logger.error("Tech Services DELETE groups failed for userId={}, entraOid={}, status={}, body={}",
-//                    userId, (entraUser != null ? entraUser.getEntraOid() : null), httpEx.getStatusCode(), errorJson, httpEx);
-//            throw new RuntimeException("Error while sending security group removal to Tech Services: " + httpEx.getStatusCode(), httpEx);
-//        } catch (Exception ex) {
-//            logger.error("Error while sending security group removal to Tech Services.", ex);
-//            throw new RuntimeException("Error while sending security group removal to Tech Services.", ex);
-//        }
+        EntraUser entraUser = null;
+        try {
+            entraUser = entraUserRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+
+            UpdateSecurityGroupsRequest request = UpdateSecurityGroupsRequest
+                    .builder()
+                    .groups(Collections.emptySet())
+                    .build();
+
+            String uri = String.format(TECH_SERVICES_UPDATE_USER_GRP_ENDPOINT, laaBusinessUnit, entraUser.getEntraOid());
+
+            logger.info("TechServices DELETE groups: userId={}, entraOid={}, uri={}", userId, entraUser.getEntraOid(), uri);
+            logger.info("Sending request to tech services to remove group memberships for deleting: {}", request);
+
+            String accessToken = getAccessToken();
+            ResponseEntity<UpdateSecurityGroupsResponse> response = restClient
+                    .patch()
+                    .uri(uri)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(request)
+                    .retrieve()
+                    .toEntity(UpdateSecurityGroupsResponse.class);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                UpdateSecurityGroupsResponse responseBody = response.getBody();
+                assert responseBody != null;
+                logger.info("Security Groups removed successfully for {} {} with security groups removed",
+                        entraUser.getFirstName() + " " + entraUser.getLastName(), responseBody.getGroupsRemoved());
+            } else {
+                logger.error("Failed to remove security groups for user {} with error code {}", entraUser.getFirstName() + " " + entraUser.getLastName(), response.getStatusCode());
+                throw new RuntimeException("Failed to remove security groups for user " + entraUser.getFirstName() + " " + entraUser.getLastName() + " with error code " + response.getStatusCode());
+            }
+        } catch (HttpClientErrorException | HttpServerErrorException httpEx) {
+            String errorJson = httpEx.getResponseBodyAsString();
+            // Do not throw error for 404, user may already be deleted in entra
+            if (httpEx.getStatusCode() == HttpStatus.NOT_FOUND) {
+                logger.warn("Tech Services DELETE groups returned 404 Not Found for userId={}, entraOid={}. User will be deleted from Silas db. Body={}",
+                        userId, (entraUser != null ? entraUser.getEntraOid() : null), errorJson);
+                return;
+            }
+            logger.error("Tech Services DELETE groups failed for userId={}, entraOid={}, status={}, body={}",
+                    userId, (entraUser != null ? entraUser.getEntraOid() : null), httpEx.getStatusCode(), errorJson, httpEx);
+            throw new RuntimeException("Error while sending security group removal to Tech Services: " + httpEx.getStatusCode(), httpEx);
+        } catch (Exception ex) {
+            logger.error("Error while sending security group removal to Tech Services.", ex);
+            throw new RuntimeException("Error while sending security group removal to Tech Services.", ex);
+        }
 
     }
 
