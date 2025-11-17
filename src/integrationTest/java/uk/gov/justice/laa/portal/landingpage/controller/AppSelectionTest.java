e435b9ddaa56c15fb44f6b62d822e991aa6cc30f
LeonardoPalmeiro <leonardo.palmeiro@justice.gov.uk>
2025-11-17 09:31:15 +0000

    add integration test AppSelectionTest

--- src/integrationTest/java/uk/gov/justice/laa/portal/landingpage/controller/AppSelectionTest.java
+++ src/integrationTest/java/uk/gov/justice/laa/portal/landingpage/controller/AppSelectionTest.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.laa.portal.landingpage.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -53,6 +54,7 @@ public class AppSelectionTest extends BaseIntegrationTest {
     private RoleAssignmentRepository roleAssignmentRepository;
 
     private AppRole testAppRole;
+    private List<AppRole> testAppRoleList;
     private int distinctIndex = 0;
 
     @BeforeEach
@@ -96,18 +98,44 @@ public class AppSelectionTest extends BaseIntegrationTest {
     }
 
     @Test
-    public void testSelectingAppsReturnsCorrectRedirectAndApps() throws Exception {
+    public void testSelectingAppsReturnsCorrectRedirectAndAppsWithOneRole() throws Exception {
         EntraUser loggedInUser = buildTestUser();
         loggedInUser = entraUserRepository.saveAndFlush(loggedInUser);
         EntraUser accessedUser = buildTestUser();
         accessedUser = entraUserRepository.saveAndFlush(accessedUser);
         UserProfile accessedUserProfile = accessedUser.getUserProfiles().stream().findFirst().orElseThrow();
         String path = String.format("/admin/users/edit/%s/apps", accessedUserProfile.getId());
-        String[] selectedApps = { UUID.randomUUID().toString() };
+        String[] selectedApps = { testAppRole.getApp().getId().toString() };
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(path)
                 .with(defaultOauth2Login(loggedInUser))
                 .with(csrf())
                 .param("apps", selectedApps))
+                .andExpect(status().is3xxRedirection())
+                .andReturn();
+        assertNotNull(result.getResponse().getRedirectedUrl());
+        assertEquals(result.getResponse().getRedirectedUrl(), String.format("/admin/users/edit/%s/roles-check-answer", accessedUserProfile.getId()));
+        HttpSession session = result.getRequest().getSession();
+        assertNotNull(session.getAttribute("selectedApps"));
+        @SuppressWarnings("unchecked")
+        List<String> returnedSelectApps = (List<String>) session.getAttribute("selectedApps");
+        assertEquals(1, returnedSelectApps.size());
+        assertEquals(selectedApps[0], returnedSelectApps.getFirst());
+    }
+
+    @Test
+    public void testSelectingAppsReturnsCorrectRedirectAndAppsWithMoreThanOneRole() throws Exception {
+        buildTestMoreThanOneAppRole();
+        EntraUser loggedInUser = buildTestUser();
+        loggedInUser = entraUserRepository.saveAndFlush(loggedInUser);
+        EntraUser accessedUser = buildTestUser();
+        accessedUser = entraUserRepository.saveAndFlush(accessedUser);
+        UserProfile accessedUserProfile = accessedUser.getUserProfiles().stream().findFirst().orElseThrow();
+        String path = String.format("/admin/users/edit/%s/apps", accessedUserProfile.getId());
+        String[] selectedApps = { testAppRoleList.getFirst().getApp().getId().toString() };
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(path)
+                        .with(defaultOauth2Login(loggedInUser))
+                        .with(csrf())
+                        .param("apps", selectedApps))
                 .andExpect(status().is3xxRedirection())
                 .andReturn();
         assertNotNull(result.getResponse().getRedirectedUrl());
@@ -135,5 +163,17 @@ public class AppSelectionTest extends BaseIntegrationTest {
         app = appRepository.saveAndFlush(app);
         AppRole appRole = buildLaaAppRole(app, "Test App Role" + distinctIndex++);
         testAppRole = appRoleRepository.saveAndFlush(appRole);
+    }
+
+    private void buildTestMoreThanOneAppRole() {
+        App app = buildLaaApp("Test App " + distinctIndex++, UUID.randomUUID().toString(),
+                "Security Group Id" + distinctIndex++, "Security Group Name" + distinctIndex++);
+        app = appRepository.saveAndFlush(app);
+        List<AppRole> roles = new ArrayList<>(
+                List.of(buildLaaAppRole(app, "Test App Role" + distinctIndex++),
+                        buildLaaAppRole(app, "Test App Role" + distinctIndex++)
+                )
+        );
+        testAppRoleList = appRoleRepository.saveAllAndFlush(roles);
     }
 }
