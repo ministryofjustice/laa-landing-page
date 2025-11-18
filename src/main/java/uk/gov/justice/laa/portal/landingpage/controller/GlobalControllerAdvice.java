b3a9b33c3c59887b398d0ffe0d75dd260f8a3860
LeonardoPalmeiro <leonardo.palmeiro@justice.gov.uk>
2025-11-18 11:43:03 +0000

    fix merge conflict

--- src/main/java/uk/gov/justice/laa/portal/landingpage/controller/GlobalControllerAdvice.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/controller/GlobalControllerAdvice.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.ClientAuthorizationRequiredException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
