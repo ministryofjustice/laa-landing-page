2ec07828c8d265b7270aa389a79011f991616c87
majid.elmi <majid.elmi@justice.gov.uk>
2026-01-26 16:26:45 +0000

    STB-3558: removed unused imports

--- src/playwrightTest/java/uk/gov/justice/laa/portal/landingpage/playwright/tests/AdminPageTest.java
+++ src/playwrightTest/java/uk/gov/justice/laa/portal/landingpage/playwright/tests/AdminPageTest.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.laa.portal.landingpage.playwright.tests;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
