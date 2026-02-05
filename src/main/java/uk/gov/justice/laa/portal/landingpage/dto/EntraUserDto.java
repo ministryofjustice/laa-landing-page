f6e4ab702e95bf07be500477569d5e2f624d68e3
Niall McMahon <niall.mcmahon@justice.gov.uk>
2026-02-05 09:58:44 +0000

    STB-3388: Fixed bug where newly created users were defaulting to having a disabled account.

--- src/main/java/uk/gov/justice/laa/portal/landingpage/dto/EntraUserDto.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/dto/EntraUserDto.java
@@ -25,5 +25,6 @@ public class EntraUserDto implements Serializable {
     private String lastName;
     private boolean multiFirmUser;
     private UserStatus userStatus;
-    private boolean enabled;
+    @Builder.Default
+    private boolean enabled = true;
 }
