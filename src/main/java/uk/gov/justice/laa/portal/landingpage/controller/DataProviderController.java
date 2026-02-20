0265ba369d3da5e83a4a3b6508217b321c17d96f
S N O W Y <49162275+snowy-snowdon@users.noreply.github.com>
2026-02-20 10:41:38 +0000

    Re-enable authorization for PDA endpoints in DataProviderController

--- src/main/java/uk/gov/justice/laa/portal/landingpage/controller/DataProviderController.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/controller/DataProviderController.java
@@ -1,6 +1,7 @@
 package uk.gov.justice.laa.portal.landingpage.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ import uk.gov.justice.laa.portal.landingpage.service.DataProviderService;
 @Slf4j
 @RestController
 @RequestMapping("/api/pda")
-// @PreAuthorize("@accessControlService.userHasAuthzRole(authentication, T(uk.gov.justice.laa.portal.landingpage.entity.AuthzRole).GLOBAL_ADMIN.roleName)")  // COMMENTED OUT FOR TESTING
+@PreAuthorize("@accessControlService.userHasAuthzRole(authentication, T(uk.gov.justice.laa.portal.landingpage.entity.AuthzRole).GLOBAL_ADMIN.roleName)")
 public class DataProviderController {
     private final DataProviderService dataProviderService;
     private final ObjectMapper objectMapper;
