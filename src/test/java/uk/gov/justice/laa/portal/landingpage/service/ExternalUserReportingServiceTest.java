b3157415bd9477ba3738c784585788ae23f987dc
Josh-Halladey <Joshua.Halladey@justice.gov.uk>
2026-03-11 14:35:30 +0000

    remove unused import

--- src/test/java/uk/gov/justice/laa/portal/landingpage/service/ExternalUserReportingServiceTest.java
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/service/ExternalUserReportingServiceTest.java
@@ -12,7 +12,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
