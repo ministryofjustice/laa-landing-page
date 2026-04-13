8afeecdbcfa43c45db139b06bf350460e10cc185
majid.elmi <majid.elmi@justice.gov.uk>
2026-04-13 13:25:15 +0100

    STB-3956: Fix persistence issue when deleting app role

--- src/main/java/uk/gov/justice/laa/portal/landingpage/service/AppRoleService.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/service/AppRoleService.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -53,7 +55,8 @@ public class AppRoleService {
     private final RoleAssignmentRepository roleAssignmentRepository;
     private final UserProfileRepository userProfileRepository;
 
-
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public List<AppRoleDto> getByIds(Collection<String> ids) {
 
@@ -142,6 +145,8 @@ public class AppRoleService {
         // Delete role permissions
         appRoleRepository.deleteRolePermissions(id);
 
+        entityManager.flush();
+        entityManager.clear();
         // Delete role
         appRoleRepository.delete(appRole);
 
