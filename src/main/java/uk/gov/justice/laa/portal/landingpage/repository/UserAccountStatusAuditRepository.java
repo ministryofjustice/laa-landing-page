550698425c3172852cbbaba094cf197449926eb3
yusuf.shah <yusuf.shah@justice.gov.uk>
2026-05-01 12:14:47 +0100

    add repository method for querying deleted users

--- src/main/java/uk/gov/justice/laa/portal/landingpage/repository/UserAccountStatusAuditRepository.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/repository/UserAccountStatusAuditRepository.java
@@ -1,9 +1,12 @@
 package uk.gov.justice.laa.portal.landingpage.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import uk.gov.justice.laa.portal.landingpage.entity.EntraUser;
+import uk.gov.justice.laa.portal.landingpage.entity.UserAccountStatus;
 import uk.gov.justice.laa.portal.landingpage.entity.UserAccountStatusAudit;
 
 import java.util.List;
@@ -19,4 +22,14 @@ public interface UserAccountStatusAuditRepository extends JpaRepository<UserAcco
         ORDER BY u.statusChangedDate DESC
         """)
     List<UserAccountStatusAudit> findByEntraUserIdOrderByStatusChangedDateDesc(@Param("entraUserId") UUID entraUserId);
+
+    @Query("""
+        SELECT u FROM UserAccountStatusAudit u
+        WHERE u.statusChange = :statusChange
+        AND (:searchTerm IS NULL OR LOWER(u.userEmail) LIKE LOWER(CONCAT('%', CAST(:searchTerm AS String), '%')))
+        """)
+    Page<UserAccountStatusAudit> findDeletedUsers(
+            @Param("statusChange") UserAccountStatus statusChange,
+            @Param("searchTerm") String searchTerm,
+            Pageable pageable);
 }
