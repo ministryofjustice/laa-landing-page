c8742dcdd77c3d82e46499e7ff49b2fb03662791
majid.elmi <majid.elmi@justice.gov.uk>
2026-05-21 17:03:30 +0100

    STB-4178: removed unused imports

--- src/main/java/uk/gov/justice/laa/portal/landingpage/controller/FirmDirectoryController.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/controller/FirmDirectoryController.java
@@ -3,7 +3,6 @@ package uk.gov.justice.laa.portal.landingpage.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -13,11 +12,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import uk.gov.justice.laa.portal.landingpage.constants.ModelAttributes;
-import uk.gov.justice.laa.portal.landingpage.dto.FirmDirectoryDto;
 import uk.gov.justice.laa.portal.landingpage.dto.FirmDirectorySearchCriteria;
 import uk.gov.justice.laa.portal.landingpage.dto.FirmDto;
 import uk.gov.justice.laa.portal.landingpage.dto.FirmOfficesCriteria;
@@ -32,7 +28,6 @@ import uk.gov.justice.laa.portal.landingpage.service.LoginService;
 import uk.gov.justice.laa.portal.landingpage.service.OfficeService;
 import uk.gov.justice.laa.portal.landingpage.service.UserAccountStatusService;
 
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
