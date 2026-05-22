89ecaa970ea66ef1ca0574eedaf9f3b364b36eca
Suresh Kavali <suresh.kavali@justice.gov.uk>
2026-05-22 11:44:57 +0100

    STB-0000 - Linter fix

--- src/main/java/uk/gov/justice/laa/portal/landingpage/utils/MaskUtil.java
+++ src/main/java/uk/gov/justice/laa/portal/landingpage/utils/MaskUtil.java
@@ -3,7 +3,9 @@ package uk.gov.justice.laa.portal.landingpage.utils;
 public class MaskUtil {
 
     public static String mask(String value) {
-        if (value == null || value.isEmpty()) return value;
+        if (value == null || value.isEmpty()) {
+            return value;
+        }
 
         int visible = 2; // number of chars to show at start/end
 
