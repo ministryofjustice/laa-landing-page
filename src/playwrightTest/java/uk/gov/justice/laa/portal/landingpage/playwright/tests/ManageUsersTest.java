978c39379d873632a1ba954d3b76e63a35099966
S N O W Y <49162275+snowy-snowdon@users.noreply.github.com>
2026-07-28 15:35:11 +0100

    Refactor import statements in ManageUsersTest for better organization

--- src/playwrightTest/java/uk/gov/justice/laa/portal/landingpage/playwright/tests/ManageUsersTest.java
+++ src/playwrightTest/java/uk/gov/justice/laa/portal/landingpage/playwright/tests/ManageUsersTest.java
@@ -6,16 +6,16 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import com.microsoft.playwright.options.AriaRole;
-import com.microsoft.playwright.options.WaitForSelectorState;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import com.microsoft.playwright.Page;
+
 import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.LoadState;
+import com.microsoft.playwright.options.WaitForSelectorState;
 
 import uk.gov.justice.laa.portal.landingpage.playwright.common.BaseFrontEndTest;
 import uk.gov.justice.laa.portal.landingpage.playwright.common.TestRole;
@@ -1501,4 +1501,3 @@ public class ManageUsersTest extends BaseFrontEndTest {
 
 
 }
-
