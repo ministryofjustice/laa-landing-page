11b1741b2079d0912282619a6697178645c0f1de
Josh-Halladey <Joshua.Halladey@justice.gov.uk>
2026-08-27 12:39:56 +0100

    fix linting

--- src/test/java/uk/gov/justice/laa/portal/landingpage/controller/UserControllerTest.java
+++ src/test/java/uk/gov/justice/laa/portal/landingpage/controller/UserControllerTest.java
@@ -191,7 +191,8 @@ class UserControllerTest {
     void setUp() {
         userController = new UserController(loginService, userService, officeService, eventService, firmService,
                 new MapperConfig().modelMapper(), accessControlService, roleAssignmentService, emailValidationService,
-                appRoleService, appService, userAccountStatusService,notificationService, userReactivationRequestService, uiLabelsProperties);
+                appRoleService, appService, userAccountStatusService, notificationService,
+                userReactivationRequestService, uiLabelsProperties);
 
         userController.disableUserFeatureEnabled = true;
         lenient().when(accessControlService.getEnablementFlags(any()))
